db3d88ac9ced146009c09034523c9ef135e2576d
amiko <basti.xachapuridze@gmail.com>
2025-09-19 20:58:16 +0400

    Add react-icons dependency, update App component structure, and enhance CSS with custom font and utility classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import Header from "./components/Header";
+
 export default function App() {
-  return <div className="text-3xl font-bold underline">Hello World</div>;
+  return (
+    <div className="flex flex-col w-full h-full bg-[#F8F6F7]">
+      <Header />
+    </div>
+  )
 }
